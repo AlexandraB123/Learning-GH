6ff9e1aa6948149cf279ad33f08a7b2ff0b173f5
Alexandra Benson <alexandrabenson123@gmail.com>
2021-07-21 15:55:32 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Alexandra Benson on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
